ecf4d19d734bef95df321e06d387d6a1f2086834
Jianqiao0412 <qiaojian@cei.cn>
2021-11-07 12:42:30 +0800

    first initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container" >
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <h1> Hello World !!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
